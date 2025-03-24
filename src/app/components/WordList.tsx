d19b8723fc82ee4d27e319d4cfb4d59a84b94741
Ido Schacham <idosius@gmail.com>
2025-03-24 15:07:40 +0100

    refactor: Pass wortschatz to WordListColumn

--- src/app/components/WordList.tsx
+++ src/app/components/WordList.tsx
@@ -8,9 +8,9 @@ type WordListProps = { wortschatz: Wortschatz }
 export default function WordList({ wortschatz }: WordListProps) {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-      <WordListColumn article="der" wortschatzEntries={wortschatz['der']} />
-      <WordListColumn article="die" wortschatzEntries={wortschatz['die']} />
-      <WordListColumn article="das" wortschatzEntries={wortschatz['das']} />
+      <WordListColumn article="der" wortschatz={wortschatz} />
+      <WordListColumn article="die" wortschatz={wortschatz} />
+      <WordListColumn article="das" wortschatz={wortschatz} />
     </div>
   )
 }
--- src/app/components/WordListColumn.tsx
+++ src/app/components/WordListColumn.tsx
@@ -1,11 +1,8 @@
-import {
-  GermanNounArticle,
-  WortschatzEntry,
-} from '@/app/types/wortilizer.types'
+import { GermanNounArticle, Wortschatz } from '@/app/types/wortilizer.types'
 
 type WordListProps = {
   article: GermanNounArticle
-  wortschatzEntries: WortschatzEntry[]
+  wortschatz: Wortschatz
 }
 
 const articleToClassName: Record<GermanNounArticle, string> = {
@@ -14,17 +11,14 @@ const articleToClassName: Record<GermanNounArticle, string> = {
   das: 'bg-green-100',
 }
 
-export default function WordListColumn({
-  article,
-  wortschatzEntries,
-}: WordListProps) {
+export default function WordListColumn({ article, wortschatz }: WordListProps) {
   return (
     <section
       className={`flex flex-col gap-2 ${articleToClassName[article]} pt-2 pb-4 px-4 rounded-2xl`}
     >
       <h2 className="text-2xl underline">{article}</h2>
       <ul className="flex flex-col gap-2">
-        {wortschatzEntries.map(({ word }) => (
+        {wortschatz[article].map(({ word }) => (
           <li key={word}>{word}</li>
         ))}
       </ul>
