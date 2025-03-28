abefcebb584a09c3da3c1641cfb60a13c76f754a
Ido Schacham <idosius@gmail.com>
2025-03-28 19:03:26 +0100

    feat: Re-introduce colored bullets and article and show empty message

--- src/app/components/WordListColumn.tsx
+++ src/app/components/WordListColumn.tsx
@@ -11,17 +11,33 @@ const articleToClassName: Record<GermanNounArticle, string> = {
   das: 'bg-green-100',
 }
 
+const articleToBullet: Record<GermanNounArticle, string> = {
+  der: '🔵',
+  die: '🔴',
+  das: '🟢',
+}
+
 export default function WordListColumn({ article, wortschatz }: WordListProps) {
+  const currentWortschatz = wortschatz[article]
+
   return (
     <section
       className={`flex flex-col gap-2 ${articleToClassName[article]} pt-2 pb-4 px-4 rounded-2xl`}
     >
-      <h2 className="text-2xl underline">{article}</h2>
-      <ul className="flex flex-col gap-2">
-        {wortschatz[article].map(({ word }) => (
-          <li key={word}>{word}</li>
-        ))}
-      </ul>
+      <header className="flex items-center justify-between">
+        <h2 className="text-2xl underline">{article}</h2>
+        <p className="text-sm">({currentWortschatz.length})</p>
+      </header>
+      {currentWortschatz.length === 0 && <p>Nothing words here yet</p>}
+      {currentWortschatz.length > 0 && (
+        <ul className="flex flex-col gap-2 max-h-[300px] overflow-y-auto">
+          {currentWortschatz.map(({ word }) => (
+            <li key={word}>
+              {articleToBullet[article]} {article} {word}
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   )
 }
