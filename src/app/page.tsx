1d67eb5646de161be58726c757e45e5fb3f09b2b
Ido Schacham <idosius@gmail.com>
2025-03-28 19:14:52 +0100

    fix: "is a client only hook" error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import Wortilizer from '@/app/components/Wortilizer'
+import { useIsClient } from '@uidotdev/usehooks'
 
 export default function Home() {
+  // Fix "is a client only hook" error
+  // Reference: https://github.com/uidotdev/usehooks/issues/218#issuecomment-1681205155
+  const isClient = useIsClient()
+
   return (
     <main className="container mx-auto px-4 py-4 md:py-8">
-      <Wortilizer />
+      {isClient && <Wortilizer />}
     </main>
   )
 }
