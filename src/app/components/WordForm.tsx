c8945d76324539ebace311ed09ebfc357667d5a6
Ido Schacham <idosius@gmail.com>
2025-05-02 16:18:22 +0200

    fix: Sanitize word before it is added

--- src/app/components/WordForm.tsx
+++ src/app/components/WordForm.tsx
@@ -7,11 +7,21 @@ type WordFormProps = {
   onAddWordAction: (word: string) => void
 }
 
+function capitalizeFirstLetter(str: string) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase()
+}
+
+function sanitizeWord(word: string) {
+  return capitalizeFirstLetter(word.trim())
+}
+
 export default function WordForm({ onAddWordAction }: WordFormProps) {
   function handleSubmit(formData: FormData) {
     const word = formData.get('word')
+
     if (word) {
-      onAddWordAction(word as string)
+      const sanitizedWord = sanitizeWord(word as string)
+      onAddWordAction(sanitizedWord)
     }
   }
 
@@ -20,11 +30,11 @@ export default function WordForm({ onAddWordAction }: WordFormProps) {
       <Input
         className="px-4"
         name="word"
-        placeholder="Deutsches Wort eingeben, z.B. Brief"
+        placeholder="Wort eingeben, z.B. Brief"
         required
       />
       <Button className="px-4 py-2" type="submit">
-        + Wort
+        + Hinzufügen
       </Button>
     </form>
   )
